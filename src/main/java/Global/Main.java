9de66b07f2b2867b6da9414df5e63be3d8a93093
PCOffline <bekerman122@gmail.com>
2019-05-11 14:13:41 +0300

    Version Alpha 3.0 Removed Command Module Added Automatic Role Updated API

--- src/main/java/Global/Main.java
+++ src/main/java/Global/Main.java
@@ -5,15 +5,12 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.RichPresence;
-import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * Main class - Boot Operations
@@ -47,26 +44,6 @@ public class Main extends ListenerAdapter {
             e.getMessage().delete().queue();
             e.getMember().getUser().openPrivateChannel().queue(channel -> channel.sendMessage("Hello master, how shall I serve you?").queue());
         }
-
-
-
-    }
-
-    @Override
-    public void onReady(ReadyEvent e) {
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                for (int i = 0; i < jda.getGuildById("531471489220870166").getMembers().size(); i++) {
-                    if (jda.getGuildById("531471489220870166").getMembers() != null && jda.getGuildById("531471489220870166").getMembers().get(i).getGame() != null && jda.getGuildById("531471489220870166").getMembers().get(i).getGame().getName().equalsIgnoreCase("Heroes & Generals") || jda.getGuildById("531471489220870166").getMembers().get(i).getGame().getName().equalsIgnoreCase("Heroes & Generals WWII") || jda.getGuildById("531471489220870166").getMembers().get(i).getGame().getName().equalsIgnoreCase("Heroes and Generals") || jda.getGuildById("531471489220870166").getMembers().get(i).getGame().getName().equalsIgnoreCase("Heroes and Generals WWII"))
-                        jda.getGuildById("531471489220870166").getController().addRolesToMember(jda.getGuildById("531471489220870166").getMembers().get(i), jda.getGuildById("531471489220870166").getRolesByName("Heroes & Generals", true).get(0)).queue();
-                    else
-                        jda.getGuildById("531471489220870166").getController().removeRolesFromMember(jda.getGuildById("531471489220870166").getMembers().get(i), jda.getGuildById("531471489220870166").getRolesByName("Heroes & Generals", true).get(0)).queue();
-                }
-
-            }
-        }, 5000);
     }
 
 }
